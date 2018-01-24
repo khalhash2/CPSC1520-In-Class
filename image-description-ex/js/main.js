7b55158b592d55352c6fe0be3f80742c3b3620e1
khalhash2 <35302426+khalhash2@users.noreply.github.com>
2018-01-24 09:31:22 -0700

    string manipulation

--- image-description-ex/js/main.js
+++ image-description-ex/js/main.js
@@ -1,13 +1,14 @@
-//select the target element
-var featureImage = document.querySelector('img.feature-hidden');
-
-//event listener function
-function featureLinkHandler(evt) {
-    
-    featureImage.src = featureLink.href;
-    featureImage.classList.remove('hidden');
+2)//declare my event handler
+function showImage(evt) {
+    //a)get a refrence to <img class="feature"> element
+    var imgElement= document.querySelector('img.feature');
+    //b) remove the hidden class from the element
+    imgElement.classList.remove('hidden');
+    //
+    var imgSrc = linkElement.href;
+    imgElement.src=imgSrc;
+    imgElement.alt = linkElement.title;
+    var captionParagrapgh = document.querySelector('p.feature.title');
+    captionParagrapgh.innerHTML = linkElement.title;
     evt.preventDefault();
-
 }
-featureImage.addEventListener('click', featureLinkHandler);
-
---
+++ string-manipulations/js/main.js
@@ -0,0 +1,35 @@
+var urlTemplate = 'http://placehold.it/:size/:color/:background/';
+var urlQueryStringTemplate = "?text="
+
+
+var textInput = document.querySelector('input[name=placeholderText]');
+
+
+function onPlaceholderTextChanged(evt) {
+    //the .targer of the event is the object that "expierenced" the event
+    console.log(evt.target.value); //value is an attribute of the <input> element
+
+    var imgElement = document.querySelector('img');
+    var src = imgElement.src;
+    var indexOfEqualSign =src.indexOf('=');
+    var newSrc = src.substring(0,indexOfEqualSign);
+    console.log(newSrc);
+    newSrc = newSrc +'='+ evt.target.value;
+    imgElement.src=newSrc;
+}
+ //wire up my event handler
+ textInput.addEventListener('change',onPlaceholderTextChanged);
+
+// mini-lesson on string manipulation:
+
+//  split string into an array
+//  join from array into a string
+//  includes / endswith / startsWith
+//  match - regular expressions
+//  indexof / lastIndexOf
+//  replace
+//  character access
+//  comparing - equals, greater/lesser
+//  slice
+//  toString
+//  trim
